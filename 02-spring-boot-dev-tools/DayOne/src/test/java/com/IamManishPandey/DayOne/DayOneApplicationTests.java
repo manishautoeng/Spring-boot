9d456deb87545a60674b2d1d394127bd2b01e667
Manish Pandey <manish.autoeng@gmail.com>
2026-06-18 09:42:11 +0530

    Add files via upload

---
+++ 02-spring-boot-dev-tools/DayOne/src/test/java/com/IamManishPandey/DayOne/DayOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IamManishPandey.DayOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
